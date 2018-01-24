f7809c95802553f40105ef7511272f43ed3c85f4
LAB_COM-MT_1 <Sirinadxnda61@gmail.com>
2018-01-24 09:22:52 +0700

    Class and Object Student Class

---
+++ src/ooplab7/Student.java
@@ -0,0 +1,69 @@
+package ooplab7;
+//talking about Student?
+//1.Student ID
+//2.Name
+//3.Address
+//4.Gender
+
+public class Student {
+    //properties of Student
+    private  String id;
+    private  String name;
+    private  String address;
+    private  String gender;
+    //Constructor
+    //Default Constructor
+    public  Student(){}
+    //Constructor by own
+    public  Student(String id, String n, String a,String g){
+        //assign data to properties
+        this.id = id;
+        this.name = n;
+        this.address = a;
+        this.gender = g;
+    }//Student
+    //getter and setter methods
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}//class
