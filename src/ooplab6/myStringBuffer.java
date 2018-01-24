29caec1ad5515164a4033942238e64df53cd1c12
LAB_COM-MT_1 <Sirinadxnda61@gmail.com>
2018-01-24 10:45:52 +0700

    myStringBuffer

---
+++ src/ooplab6/myStringBuffer.java
@@ -0,0 +1,26 @@
+package ooplab6;
+
+public class myStringBuffer {
+    public class myStringBuffer {
+        public static void main(String[] args) {
+//        StringBuffer
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("Hello");
+            System.out.println(buffer);
+            buffer.append(20);
+            System.out.println(buffer);
+            buffer.insert(5," ");
+            System.out.println(buffer);
+            System.out.println(buffer.reverse());
+//            System.out.println(buffer.reverse());
+            buffer.append(" RMUTSV");
+            System.out.println(buffer);
+            buffer.delete(6, 8);
+            System.out.println(buffer);
+            pringBuffer(buffer);
+        }//main
+
+        private static void pringBuffer(StringBuffer buffer) {
+            System.out.println(buffer);
+        }
+    }//class
