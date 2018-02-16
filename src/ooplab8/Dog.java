150db6766832ae3d31eb2c0ec5e7e3099304c1cb
sirinad10 <sirinadxnda61@gmail.com>
2018-02-16 19:43:09 +0700

    Dog

--- src/ooplab8/Dog.java
+++ src/ooplab8/Dog.java
@@ -1,7 +1,12 @@
-package ooplab8;
+package opplab8;
 
-public class Dog extends Pet{
+public class Dog extends Pet  {
+    public Dog(String name, int age) {
+        super(name,age);
+    }
+
     @Override
-    public void makeNoise(
-    )
+    public void makeNoise() {
+        System.out.println("Box Box !!!");
+    }
 }
